a7657309e3c07d7cd7bde01394ec538fb5ecb357
gabisena <fraugabrielasena@gmail.com>
2021-07-11 13:00:11 +0200

    semi final changes

--- beachblue-app/beachblue.js
+++ beachblue-app/beachblue.js
@@ -1,4 +1,23 @@
 
+//function to sort the JSON based on parameter
+function sortJSON(arr, key, way) {
+  return arr.sort(function(a, b) {
+      let x = a[key]; let y = b[key];
+      if (way === 'asc') { return ((x < y) ? -1 : ((x > y) ? 1 : 0)); }
+      if (way === 'desc') { return ((x > y) ? -1 : ((x < y) ? 1 : 0)); }
+  });
+}
+
+function cardFill(json_beach){
+  for (let i = 0; i < 3; i++) {
+  document.getElementById("card-" + (i+1)).getElementsByClassName("city-country")[0].textContent = json_beach[i]["Province"] + ", " + json_beach[i]["Community"]
+  document.getElementById("card-" + (i+1)).getElementsByClassName("lake-beach")[0].textContent = json_beach[i]["Name"]
+  document.getElementById("card-" + (i+1)).getElementsByClassName("description")[0].textContent = json_beach[i]["Descripci"].substring(0,75) + "..."
+  document.getElementById("card-" + (i+1)).getElementsByClassName("image")[0].src =  "./styles/img/beach-"+ json_beach[i]["Unnamed: 0"] + ".jpeg";
+  }
+}
+
+// fetch the new_playas.json file and create a variabe called json_beach
   function fetchJson(){
     fetch('/playas_rating.json')
     .then(response => {
@@ -12,31 +31,18 @@
       this.users = json;
       json_beach = this.users
       json_beach = sortJSON(json_beach,'ratings_2','desc')
-      console.log("vai retornar o json")
+      cardFill(json_beach)
       return json_beach
     })
     .catch(function () {
       this.dataError = true;
     })
+    //return json_beach
   }
-// fetch the new_playas.json file and create a variabe called json_beach
 
+fetchJson();
 
-//function to sort the JSON based on parameter
-      function sortJSON(arr, key, way) {
-        return arr.sort(function(a, b) {
-            let x = a[key]; let y = b[key];
-            if (way === 'asc') { return ((x < y) ? -1 : ((x > y) ? 1 : 0)); }
-            if (way === 'desc') { return ((x > y) ? -1 : ((x < y) ? 1 : 0)); }
-        });
-      }
-
-function cardFill(json_beach){
-  document.getElementById("card-1").getElementsByClassName("city-country")[0].textContent = json_beach[0]["Province"] + ", " + json_beach[0]["Community"]
-  document.getElementById("card-1").getElementsByClassName("lake-beach")[0].textContent = json_beach[0]["Name"]
-  document.getElementById("card-1").getElementsByClassName("description")[0].textContent = json_beach[0]["Descripci"].substring(0,75) + "..."
-  document.getElementById("card-1").getElementsByClassName("image")[0].src =  "./styles/img/beach-"+ json_beach[0]["Unnamed: 0"] + ".jpeg";
-  
+function storeTerm(){
+  localStorage.setItem("termSearch",document.getElementById("search-text").value);
 }
-let test = fetchJson();
-console.log(fetch('/playas_rating.json'))
+
